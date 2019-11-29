34049ffc0ef6ed20388f0018c68b430e3204cb11
Sheetal Bedarkar <sheetalbedarkar96@gmail.com>
2019-11-29 12:41:48 +0530

    given max num at 2nd position return same num

--- src/test/java/com/bridgelabz/genericsTest/FindMaxNumberTest.java
+++ src/test/java/com/bridgelabz/genericsTest/FindMaxNumberTest.java
@@ -7,12 +7,22 @@ import org.junit.Test;
 public class FindMaxNumberTest {
 
     @Test
-    public void givenThreeInput_WhenMaximumNumber_shouldReturnTrue() {
+    public void givenThreeInput_WhenMaximumNumber_onFirstPosition_shouldReturnTrue() {
         Integer a = 10;
         Integer b = 7;
         Integer c = 8;
         FindMaxNumber findMaxNum = new  FindMaxNumber();
         Integer maxNum = findMaxNum.findMaxInteger(a,b,c);
         Assert.assertEquals((Integer)10,maxNum);
-        }
+    }
+
+    @Test
+    public void givenThreeInput_whenMaxNumber_onSecondPosition_shouldReturnTrue() {
+        Integer a = 1;
+        Integer b = 8;
+        Integer c = 4;
+        FindMaxNumber findMaxNum = new  FindMaxNumber();
+        Integer maxNum = findMaxNum.findMaxInteger(a,b,c);
+        Assert.assertEquals((Integer)8,maxNum);
+    }
 }
